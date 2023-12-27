6ee86f80bf41a8f3dc4030f0e88282f286c280de
Gustavo Breternitz <breternitzgustavo@gmail.com>
2023-12-27 15:46:53 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar, SafeAreaView, View, StyleSheet } from 'react-native';
+import Cesta from './src/Views/Cesta';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold} from  '@expo-google-fonts/montserrat'
 export default function App() {
+  const [fontLoaded] = useFonts({
+    "regular": Montserrat_400Regular,
+    "bold": Montserrat_700Bold
+  });
+
+  if (!fontLoaded) {
+    return <View />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // SafeAreaView é bom para o IOS pois ele não tem uma barra fixa 
+    // e aqui consideramos apenas o disponível para texto
+      <SafeAreaView>
+        <StatusBar barStyle="dark-content" />
+        <Cesta />
+      </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
